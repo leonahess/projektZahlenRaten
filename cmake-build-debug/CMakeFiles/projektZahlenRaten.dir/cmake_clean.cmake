2eb567f38a4923715f4ccf4bd8a83154034e57e2
Leon <leonhess@github.com>
2017-11-15 16:22:33 +0100

    Projekt erstellt

---
+++ cmake-build-debug/CMakeFiles/projektZahlenRaten.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projektZahlenRaten.dir/main.cpp.o"
+  "projektZahlenRaten.pdb"
+  "projektZahlenRaten"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projektZahlenRaten.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
